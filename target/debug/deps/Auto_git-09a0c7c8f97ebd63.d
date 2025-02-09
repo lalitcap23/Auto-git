c05057490416ab6fd0cfc3161ff02d7e06852741
lalitcap23 <lalitrajput232002@gmail.com>
2025-02-09 20:36:45 +0530

    some changed

---
+++ target/debug/deps/Auto_git-09a0c7c8f97ebd63.d
@@ -0,0 +1,5 @@
+/home/lalit/rust/folder/hello/Auto-git/target/debug/deps/libAuto_git-09a0c7c8f97ebd63.rmeta: src/main.rs
+
+/home/lalit/rust/folder/hello/Auto-git/target/debug/deps/Auto_git-09a0c7c8f97ebd63.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/dotenvy-dd2cace55f81f9a1.d
@@ -0,0 +1,9 @@
+/home/lalit/rust/folder/hello/Auto-git/target/debug/deps/libdotenvy-dd2cace55f81f9a1.rmeta: /home/lalit/.cargo/registry/src/index.crates.io-6f17d22bba15001f/dotenvy-0.15.7/src/lib.rs /home/lalit/.cargo/registry/src/index.crates.io-6f17d22bba15001f/dotenvy-0.15.7/src/errors.rs /home/lalit/.cargo/registry/src/index.crates.io-6f17d22bba15001f/dotenvy-0.15.7/src/find.rs /home/lalit/.cargo/registry/src/index.crates.io-6f17d22bba15001f/dotenvy-0.15.7/src/iter.rs /home/lalit/.cargo/registry/src/index.crates.io-6f17d22bba15001f/dotenvy-0.15.7/src/parse.rs
+
+/home/lalit/rust/folder/hello/Auto-git/target/debug/deps/dotenvy-dd2cace55f81f9a1.d: /home/lalit/.cargo/registry/src/index.crates.io-6f17d22bba15001f/dotenvy-0.15.7/src/lib.rs /home/lalit/.cargo/registry/src/index.crates.io-6f17d22bba15001f/dotenvy-0.15.7/src/errors.rs /home/lalit/.cargo/registry/src/index.crates.io-6f17d22bba15001f/dotenvy-0.15.7/src/find.rs /home/lalit/.cargo/registry/src/index.crates.io-6f17d22bba15001f/dotenvy-0.15.7/src/iter.rs /home/lalit/.cargo/registry/src/index.crates.io-6f17d22bba15001f/dotenvy-0.15.7/src/parse.rs
+
+/home/lalit/.cargo/registry/src/index.crates.io-6f17d22bba15001f/dotenvy-0.15.7/src/lib.rs:
+/home/lalit/.cargo/registry/src/index.crates.io-6f17d22bba15001f/dotenvy-0.15.7/src/errors.rs:
+/home/lalit/.cargo/registry/src/index.crates.io-6f17d22bba15001f/dotenvy-0.15.7/src/find.rs:
+/home/lalit/.cargo/registry/src/index.crates.io-6f17d22bba15001f/dotenvy-0.15.7/src/iter.rs:
+/home/lalit/.cargo/registry/src/index.crates.io-6f17d22bba15001f/dotenvy-0.15.7/src/parse.rs:
